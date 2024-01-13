45c4ca367f0454fc5761c57bd24d2cac0ee628df
Iliyan <alibhai@MacBook-Pro-11.local>
2024-01-12 21:31:58 -0800

    Completed Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <button
+      className="bg-rishi-white border border-rishi-orange text-rishi-black font-urbanist py-2 px-4 rounded"
+      style={{
+        width: "238px",
+        height: "46px",
+        borderRadius: "7px",
+      }}
+    >
+      Back to Home
+    </button>
+  );
 };
 
 export default Button;
