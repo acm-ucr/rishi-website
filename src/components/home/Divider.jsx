a88611842690f0c363209a7ab1f68985aab1926e
kevin <kevincao64@gmail.com>
2024-01-12 23:27:25 -0800

    added changes to divider

--- src/components/home/Divider.jsx
+++ src/components/home/Divider.jsx
@@ -4,18 +4,18 @@ import profile from "@/public/prishi-logo.webp";
 const Divider = () => {
   return (
     <div className="flex items-center h-auto w-full">
-      <div className="flex flex-col items-end w-3/5 ">
+      <div className="flex flex-col items-end gap-4 w-3/5">
         <hr className="w-1/2 border-rishi-orange" />
-        <hr className="w-1/5 border-rishi-green mt-4" />
+        <hr className="w-1/5 border-rishi-green" />
       </div>
 
-      <div className="flex flex-col h-24 w-auto items-center px-11 mt-12">
+      <div className="flex flex-col w-auto items-center px-11">
         <Image src={profile} alt="rishi-logo" className="w-24 h-auto" />
       </div>
 
-      <div className="flex flex-col items-start w-3/5">
+      <div className="flex flex-col items-start gap-4 w-3/5">
         <hr className="w-1/2 border-rishi-orange" />
-        <hr className="w-1/5 border-rishi-green mt-4" />
+        <hr className="w-1/5 border-rishi-green" />
       </div>
     </div>
   );
