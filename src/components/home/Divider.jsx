87a6b066869e5ac1c9e88c7cf9e6c53b4e136418
theo <theobfernandez@gmail.com>
2024-01-13 15:01:45 -0800

    merge dev

--- src/components/home/Divider.jsx
+++ src/components/home/Divider.jsx
@@ -3,13 +3,13 @@ import profile from "@/public/prishi-logo.webp";
 
 const Divider = () => {
   return (
-    <div className="flex items-center h-auto w-full">
+    <div className="flex items-center">
       <div className="flex flex-col items-end gap-4 w-3/5">
         <hr className="w-1/2 border-rishi-orange" />
         <hr className="w-1/5 border-rishi-green" />
       </div>
 
-      <div className="flex flex-col w-auto items-center px-11">
+      <div className="flex flex-col items-center px-11">
         <Image src={profile} alt="rishi-logo" className="w-24 h-auto" />
       </div>
 
