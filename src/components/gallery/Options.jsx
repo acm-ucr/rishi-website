749277b99f5b3051d15224e23eacb6c38f02d420
Vidhi Tapde <vidhitapde@vidhis-mbp.lan>
2024-01-14 00:38:46 -0800

    vidhitapde/options

--- src/components/gallery/Options.jsx
+++ src/components/gallery/Options.jsx
@@ -1,5 +1,41 @@
+import React from "react";
+import Button from "../Button";
 const Options = () => {
-  return <div>Options</div>;
+  return (
+    <div className="flex flex-col items-center ">
+      <div className="mb-2">
+        <Button
+          text="INDIA TRIP 2014"
+          href={"https://www.google.com/"}
+          color="orange"
+        />
+      </div>
+
+      <div className="mb-2">
+        <Button
+          text="INDIA TRIP 2016"
+          href={"https://www.google.com/"}
+          color="green"
+        />
+      </div>
+
+      <div className="mb-2">
+        <Button
+          text="INDIA TRIP 2019"
+          href={"https://www.google.com/"}
+          color="orange"
+        />
+      </div>
+
+      <div className="mb-2">
+        <Button
+          text="PAD SHIPMENT 2022"
+          href={"https://www.google.com/"}
+          color="green"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Options;
