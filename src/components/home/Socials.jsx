ddb3a9f85c13f3778751e068f845810818f8635c
Recogn1z <543257239@qq.com>
2024-01-13 19:09:58 -0800

    scott third commit

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -6,18 +6,18 @@ import { FaTiktok } from "react-icons/fa";
 import Link from "next/link";
 const Socials = () => {
   return (
-    <div className="flex justify-center flex-col items-center">
-      <div className="flex  justify-center  gap-20">
+    <div className="flex justify-center flex-col items-center h-screen w-full">
+      <div className="flex  justify-around gap-2  w-11/12 h-2/5">
         <Link
           href="https://www.instagram.com/ucrprishi/?hl=en"
           target="_blank"
-          className="border-2 border-rishi-green  rounded-2xl flex justify-center items-center w-60  h-64 "
+          className="border-2 border-rishi-green  rounded-2xl flex justify-center items-center w-1/5 h-full  "
         >
-          <div className="shadow bg-rishi-white rounded-2xl  w-56 py-3  h-60 flex flex-col justify-around items-center">
-            <div className="flex flex-row  justify-center  ">
-              <AiFillInstagram className=" w-24 h-24 text-rishi-black bg-rishi-white" />
+          <div className="shadow bg-rishi-white rounded-2xl   w-11/12 h-5/6 flex flex-col justify-around items-center  ">
+            <div className="flex flex-row  justify-center items-center  w-1/2 h-2/3 pt-3 ">
+              <AiFillInstagram className="  text-rishi-black bg-rishi-white w-full h-full" />
             </div>
-            <div className="flex  font-bold   text-2xl w-28 h-8 items-center justify-center text-rishi-black">
+            <div className="flex  font-bold h-full w-full text-2xl  items-center justify-center text-rishi-black pb-4  ">
               Instagram
             </div>
           </div>
@@ -26,13 +26,13 @@ const Socials = () => {
         <Link
           href="https://www.facebook.com/ucrprishi/"
           target="_blank"
-          className="border-2 border-rishi-orange rounded-2xl flex justify-center items-center w-60  h-64 "
+          className="border-2 border-rishi-orange rounded-2xl flex justify-center items-center w-1/5 h-full"
         >
-          <div className="shadow bg-rishi-white rounded-2xl py-3 w-56 h-60 flex flex-col justify-around items-center">
-            <div className="flex flex-row  justify-center ">
-              <FaFacebookSquare className=" w-24 h-24 fill-rishi-black" />
+          <div className="shadow bg-rishi-white rounded-2xl  flex flex-col justify-around items-center w-11/12 h-5/6 ">
+            <div className="flex flex-row  justify-center items-center  w-1/2 h-2/3 pt-3  ">
+              <FaFacebookSquare className="  fill-rishi-black w-full h-full" />
             </div>
-            <div className="  flex  font-bold   text-2xl w-28 h-8 items-center justify-center text-rishi-black">
+            <div className="  flex  font-bold   text-2xl  items-center justify-center text-rishi-black h-full w-full pb-4 ">
               Facebook
             </div>
           </div>
@@ -41,13 +41,13 @@ const Socials = () => {
         <Link
           href="https://ucr.campuslabs.com/engage/organization/projectrishi"
           target="_blank"
-          className="border-2 border-rishi-green rounded-2xl flex justify-center items-center w-60  h-64 "
+          className="border-2 border-rishi-green rounded-2xl flex justify-center items-center w-1/5 h-full "
         >
-          <div className="shadow bg-rishi-white rounded-2xl w-56 h-60 py-3 flex flex-col justify-around items-center">
-            <div className="flex flex-row  justify-center ">
-              <FaLink className="w-24 h-24 fill-rishi-black" />
+          <div className="shadow bg-rishi-white rounded-2xl  flex flex-col justify-around   items-center w-11/12 h-5/6 ">
+            <div className="flex flex-row  justify-center items-center  w-1/2 h-2/3 pt-3 ">
+              <FaLink className=" fill-rishi-black w-full h-full" />
             </div>
-            <div className="flex  font-bold   text-2xl  w-48 h-9 items-center justify-center text-rishi-black">
+            <div className="flex  font-bold   text-2xl  items-center justify-center text-rishi-black h-full w-full pb-4 ">
               Highlander Link
             </div>
           </div>
@@ -56,28 +56,28 @@ const Socials = () => {
         <Link
           href="https://www.tiktok.com/@ucrprishi?_t=8bass9dmqp8&_r=1"
           target="_blank"
-          className="border-2 border-rishi-orange rounded-2xl flex justify-center items-center w-60  h-64 "
+          className="border-2 border-rishi-orange rounded-2xl flex justify-center items-center w-1/5 h-full "
         >
-          <div className="shadow bg-rishi-white rounded-2xl py-3 w-56 h-60 flex flex-col justify-around items-center">
-            <div className="flex flex-row  justify-center ">
-              <FaTiktok className="w-24 h-24 fill-rishi-black" />
+          <div className="shadow bg-rishi-white rounded-2xl   flex flex-col justify-around items-center  w-11/12 h-5/6">
+            <div className="flex flex-row  justify-center  w-1/2 h-2/3 pt-3 ">
+              <FaTiktok className=" fill-rishi-black  w-full h-full" />
             </div>
-            <div className="flex  w-28 h-8 items-center font-bold   text-2xl justify-center text-rishi-black">
+            <div className="flex  items-center font-bold   text-2xl justify-center text-rishi-black h-full w-full pb-4 ">
               Tiktok
             </div>
           </div>
         </Link>
       </div>
-      <a
+      <Link
         href="https://docs.google.com/forms/d/e/1FAIpQLSeFIXoOH9AaAT4KrjpJJAajZ-8rP4sDbhGCNkcDbcZ0uXer5Q/viewform"
         target="_blank"
-        className="flex flex-row justify-center text-rishi-orange w-96 h-12  items-center  mt-14 font-bold  text-2xl gap-2"
+        className="flex flex-row justify-center text-rishi-orange   items-center   font-bold  text-2xl gap-2 w-full h-1/4 "
       >
-        <div className=" w-10 h-10 ">
-          <MdMail className="text-rishi-orange bg-rishi-white w-full h-full" />
+        <div className="w-full h-full flex flex-row items-center justify-center">
+          <MdMail className="text-rishi-orange bg-rishi-white " />
+          JOIN OUR MAILING LIST!
         </div>
-        JOIN OUR MAILING LIST!
-      </a>
+      </Link>
     </div>
   );
 };
