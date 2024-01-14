84f54180b8128e2115d2971e78154b7a1e634aca
Iliyan <alibhai@MacBook-Pro-11.local>
2024-01-13 18:19:09 -0800

    added hover and fixed border dimensions, removed py-2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,12 @@ import "tailwindcss/tailwind.css";
 const Button = () => {
   return (
     <button
-      className="bg-rishi-white border border-rishi-orange text-rishi-black font-urbanist py-2 px-4 rounded"
-      style={{
-        width: "238px",
-        height: "46px",
-        borderRadius: "7px",
-      }}
+      className="bg-rishi-white border border-rishi-orange text-rishi-black font-urbanist border-2 px-6 rounded-md px-4 rounded hover:bg-rishi-orange"
+      // style={{
+      //   width: "238px",
+      //   height: "46px",
+      //   borderRadius: "7px",
+      // }}
     >
       Back to Home
     </button>
