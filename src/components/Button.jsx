db4ad2942ebcdc1ea0ef23cc718a1f08a11a39b5
Iliyan <alibhai@MacBook-Pro-11.local>
2024-01-13 18:23:45 -0800

    Added hover, removed px-2, fixed border size, deleted inline css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,7 @@ import "tailwindcss/tailwind.css";
 
 const Button = () => {
   return (
-    <button
-      className="bg-rishi-white border border-rishi-orange text-rishi-black font-urbanist border-2 px-6 rounded-md px-4 rounded hover:bg-rishi-orange"
-      // style={{
-      //   width: "238px",
-      //   height: "46px",
-      //   borderRadius: "7px",
-      // }}
-    >
+    <button className="bg-rishi-white border border-rishi-orange text-rishi-black font-urbanist border-2 px-6 rounded-md px-4 rounded hover:bg-rishi-orange">
       Back to Home
     </button>
   );
