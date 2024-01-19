b509fb7fba3430c92f88bdbd165c6e178e88f5a8
syedmahdi30 <syedmahdi30@gmail.com>
2024-01-19 13:15:29 -0800

    Used tailwind and added rishi-website colors

--- src/components/board/Member.jsx
+++ src/components/board/Member.jsx
@@ -7,41 +7,11 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 
 const Member = () => {
-  const iconStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    width: "200px",
-    height: "300px",
-    backgroundColor: "white",
-    borderRadius: "15px",
-    color: "white",
-    boxShadow: "0px 6px 10px rgba(0, 0, 0, 0.3)",
-  };
-
-  const containerStyle = {
-    display: "flex",
-    justifyContent: "center",
-  };
-
-  const imageContainerStyle = {
-    borderRadius: "15px",
-    overflow: "hidden",
-    padding: "10px",
-    marginTop: "10px",
-  };
-
-  const imageWrapperStyle = {
-    borderRadius: "15px",
-    overflow: "hidden",
-  };
-
   return (
-    <div style={containerStyle}>
-      <div style={iconStyle}>
-        <div style={imageContainerStyle}>
-          <div style={imageWrapperStyle}>
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center justify-start w-48 h-72 bg-rishi-white rounded-xl text-rishi-white shadow-2xl ring-0.5 ring-rishi-black">
+        <div className="rounded-lg overflow-hidden p-2 mt-2">
+          <div className="rounded-lg overflow-hidden">
             <Image
               src={placeholder}
               alt="Member"
@@ -52,18 +22,20 @@ const Member = () => {
           </div>
         </div>
         <div className="flex flex-col items-start -mt-2">
-          <p className="text-2xl font-urbanist font-bold text-orange-500 pr-7 tracking-wide">
+          <p className="text-2xl font-urbanist font-bold text-rishi-orange pr-7 tracking-wide">
             Jennie Kim
           </p>
-          <p className="text-lg font-urbanist text-black pr-19">President</p>
+          <p className="text-lg font-urbanist text-rishi-black pr-19 mt-[-5px]">
+            President
+          </p>
           <div className="flex">
-            <FaLinkedinIn className="h-6 w-6 text-orange-500" />
-            <IoMdMail className="h-6 w-6 text-orange-500 ml-2" />
+            <FaLinkedinIn className="h-6 w-6 text-rishi-orange" />
+            <IoMdMail className="h-6 w-6 text-rishi-orange ml-2" />
           </div>
-          <p className="italic text-xs font-urbanist text-black mt-1 -ml-1">
+          <p className="text-xs font-urbanist text-rishi-black mt-1 -ml-1">
             3rd year - Comp Sci
           </p>
-          <p className="italic text-xs font-urbanist text-black mt-0.5 -ml-1">
+          <p className="text-xs font-urbanist text-rishi-black mt-0.5 -ml-1">
             Pronouns: She/Her/Hers
           </p>
         </div>
