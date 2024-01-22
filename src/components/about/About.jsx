24e21b05849f6922f6bff69df2ed3663d833f0cf
Iliyan <alibhai@MacBook-Pro-11.local>
2024-01-21 17:40:18 -0800

    working on dimensions for paragph in 1st div

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,34 @@
+import React from "react";
 const About = () => {
-  return <div>About</div>;
+  return (
+    // parent div
+    <div className="flex flex-col">
+      <p></p>
+      {}
+      <div className="border p-4 w-1082 h-331 top-433 left-226"> </div>
+      <p className="font-Urbanist text-xl font-normal leading-7 tracking-normal text-center">
+        Project RISHI is a nonprofit organization with the mission of promoting
+        the sustainable growth and development of the Indian village of Ghangar.
+      </p>
+      {}
+      <div
+        className="border p-4"
+        style={{
+          // Figma dimensions
+          width: "997px",
+          height: "569px",
+          top: "669px",
+          left: "286px",
+          borderRadius: "500px",
+        }}
+      >
+        {}
+      </div>
+
+      {}
+      <div className="border p-4">{}</div>
+    </div>
+  );
 };
 
 export default About;
