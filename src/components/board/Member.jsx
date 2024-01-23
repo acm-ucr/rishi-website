5cb85063d2d7652afdfacbb1572ee6bfeba8dd9e
syedmahdi30 <syedmahdi30@gmail.com>
2024-01-22 18:40:35 -0800

    Fixed unnecessary padding and simplified code

--- src/components/board/Member.jsx
+++ src/components/board/Member.jsx
@@ -9,8 +9,8 @@ import { IoMdMail } from "react-icons/io";
 const Member = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col items-center justify-start w-48 h-72 bg-rishi-white rounded-xl text-rishi-white shadow-2xl ring-0.5 ring-rishi-black">
-        <div className="rounded-lg overflow-hidden p-2 mt-2">
+      <div className="flex gap-3 p-5 flex-col items-start justify-start  bg-rishi-white rounded-xl text-rishi-white shadow-2xl ring-0.5 ring-rishi-black">
+        <div className="rounded-lg overflow-hidden mt-2">
           <div className="rounded-lg overflow-hidden">
             <Image
               src={placeholder}
@@ -21,21 +21,21 @@ const Member = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col items-start -mt-2">
-          <p className="text-2xl font-urbanist font-bold text-rishi-orange pr-7 tracking-wide">
+        <div className="flex flex-col -mt-2">
+          <p className="text-2xl font-urbanist font-bold text-rishi-orange  tracking-wide">
             Jennie Kim
           </p>
-          <p className="text-lg font-urbanist text-rishi-black pr-19 mt-[-5px]">
+          <p className="text-lg font-urbanist text-rishi-black mt-[-5]">
             President
           </p>
           <div className="flex">
             <FaLinkedinIn className="h-6 w-6 text-rishi-orange" />
             <IoMdMail className="h-6 w-6 text-rishi-orange ml-2" />
           </div>
-          <p className="text-xs font-urbanist text-rishi-black mt-1 -ml-1">
+          <p className="text-xs font-urbanist text-rishi-black mt-1 p-0">
             3rd year - Comp Sci
           </p>
-          <p className="text-xs font-urbanist text-rishi-black mt-0.5 -ml-1">
+          <p className="text-xs font-urbanist text-rishi-black p-0 mt-0.5">
             Pronouns: She/Her/Hers
           </p>
         </div>
