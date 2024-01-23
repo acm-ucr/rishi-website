a7461d8c3099e240d4f27099ae49c20240b19388
ashleyvnguyen25 <ashley.v.nguyen@gmail.com>
2024-01-23 00:10:56 -0800

    mission

--- src/components/home/Mission.jsx
+++ src/components/home/Mission.jsx
@@ -1,32 +1,38 @@
 const Mission = () => {
-  return ( 
-  <div>Mission
-  
-    <div className="flex">
-      <div className="bg-rishi-orange" style={{flex: 1, width:'50%',height:'438px'}}>
-        <div className="bg-rishi-orange" style={{width:'477px',height:'130px', top:'1161px',left:'-51px'}}> 
-          <div className="text-rishi-white text-4xl" style={{lineHeight:'48px', fontWeight:'700'}}>Objective</div>
-        
-        </div>
-        
-        <div className="bg-rishi-orange" style={{width:'589px',height:'270px'}}> 
-          <div className="text-rishi-white text-2xl"style={{lineHeight:'38.5px', fontWeight:'400'}}> Through our fundraising events like Holi and Garba, we have had the opportunity to support this amazing village in Ghangar, India. The locals living there are strong, hardworking, and high-spirited. Together, we will continue to put efforts towards strengthening this community & we thank you all for participating in our events thus far! </div>
-        </div>
-      
-      </div>
-    
-      <div className="bg-rishi-green" style={{flex:1, width:'50%',height:'438px', position:'absolute', right:0}}>
-        <div className="bg-rishi-green" style={{width:'477px',height:'130px'}}> 
-          <div className="text-rishi-white text-4xl"style={{lineHeight:'48px', fontWeight:'700'}}>UCR Chapter</div>
+  return (
+    <div>
+      Mission
+      <div className="flex">
+        <div className="bg-rishi-orange w-1/2 flex flex-col gap-6 p-10">
+          <div className="text-left font-bold text-rishi-white text-4xl">
+            Objective
+          </div>
+
+          <div className="text-rishi-white text-2xl leading-loose">
+            {" "}
+            Through our fundraising events like Holi and Garba, we have had the
+            opportunity to support this amazing village in Ghangar, India. The
+            locals living there are strong, hardworking, and high-spirited.
+            Together, we will continue to put efforts towards strengthening this
+            community & we thank you all for participating in our events thus
+            far!{" "}
+          </div>
         </div>
 
-        <div className="bg-rishi-green" style={{width:'589px',height:'193px'}}> 
-          <div className="text-rishi-white text-2xl"style={{lineHeight:'38.5px', fontWeight:'400'}}> At the University of California, Riverside, we are committed to supporting the village of Ghangar in Uttarakhand, India. We take a holistic approach to supporting our village by focusing on improving sustainability, quality of life, & women’s empowerment.</div>
+        <div className="bg-rishi-green w-1/2 flex flex-col gap-6 p-10">
+          <div className="text-left font-bold text-rishi-white text-4xl">
+            UCR Chapter
+          </div>
+          <div className="text-rishi-white text-2xl leading-loose">
+            {" "}
+            At the University of California, Riverside, we are committed to
+            supporting the village of Ghangar in Uttarakhand, India. We take a
+            holistic approach to supporting our village by focusing on improving
+            sustainability, quality of life, & women’s empowerment.
+          </div>
         </div>
       </div>
     </div>
-
-  </div>
   );
 };
 
