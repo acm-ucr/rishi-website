99ed2cddfea82d075fdb14b876fc21f8a05654f6
Iliyan <alibhai@MacBook-Pro-11.local>
2024-01-28 00:26:05 -0800

    need help with 2nd and 3rd div spacing

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
     // parent div with flex column
     <div className="flex justify-center">
       <div className="w-11/12 flex gap-2 text-rishi-black justify-center flex-col p-2">
-        {/* First container */}
+        {}
         <div className="font-Urbanist  text-xl font-normal leading-7 tracking-normal text-center p-4">
           Project RISHI is a nonprofit organization with the mission of <br />
           promoting the sustainable growth and development of the Indian village
           <br />
           of Ghangar.
         </div>
-        {/* Second container with image */}
+        {}
         <div className="flex justify-center m-0 p-0">
           <Image
             src={aboutImage}
