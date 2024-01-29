236dac2fa3db466142fd8c287bbbef00ff6d5ee4
Ricardo Galeano <ricardogaleano@ucr-secure-46-10-13-232-248.wnet.ucr.edu>
2024-01-29 12:29:48 -0800

    fixing more bugs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,49 +14,45 @@ const Navigation = () => {
       collapseOnSelect
       expand="md"
       fixed="top"
-      className="bg-rishi-white p-4"
+      className="bg-rishi-white p-4 flex justify-between items-center"
     >
       <Navbar.Brand>
         <Link eventkey="1" className="p-0 flex" href="/">
           <Image
             src={logo}
             className=" flex justify-between object-scale-down object-left h-[57px]"
+            alt="Rishi-Logo"
           ></Image>
-
-          <Navbar.Toggle
-            className="list-unstyled !text-transparent border-0"
-            aria-controls="basic-navbar-nav"
-          ></Navbar.Toggle>
-
-          <Navbar.Collapse>
-            <Nav className="w-12/12 flex items-center gap-x-10">
-              {items.map((item, index) => (
-                <Link
-                  key={index}
-                  href={item.link}
-                  onClick={() => setSelected(item.name)}
-                >
-                  <div className="flex-col items-center">
-                    <div className=" font-urbanist font-light text-rishi-green hover:text-rishi-orange p-4 text-2xl">
-                      {item.name}
-                    </div>
-
-                    <div className="flex justify-center">
-                      <div
-                        className={
-                          selected === item.name
-                            ? " bg-rishi-orange p-1 -translate-y-3 rounded-full"
-                            : "p-1"
-                        }
-                      ></div>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </Nav>
-          </Navbar.Collapse>
         </Link>
       </Navbar.Brand>
+
+      <Navbar.Collapse>
+        <Nav className="w-12/12 flex items-center gap-x-10">
+          {items.map((item, index) => (
+            <Link
+              key={index}
+              href={item.link}
+              onClick={() => setSelected(item.name)}
+            >
+              <div className="flex-col items-center">
+                <div className=" font-urbanist font-light text-rishi-green hover:text-rishi-orange p-4 text-2xl">
+                  {item.name}
+                </div>
+
+                <div className="flex justify-center">
+                  <div
+                    className={
+                      selected === item.name
+                        ? " bg-rishi-orange p-1 -translate-y-3 rounded-full"
+                        : "p-1"
+                    }
+                  ></div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
