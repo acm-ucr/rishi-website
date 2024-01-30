bc8ec91ac9f80d39c9477fd48f105f7f1a05aa13
theo <theobfernandez@gmail.com>
2024-01-30 00:14:35 -0800

    v

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,20 +5,18 @@ import aboutImage from "@/public/about.webp";
 const About = () => {
   return (
     // parent div with flex column
-    <div className="flex justify-center">
-      <div className="w-11/12 flex gap-2 text-rishi-black items-center flex-col p-2 justify-center">
-        <div className="w-1/2 font-Urbanist  text-xl font-normal leading-7 tracking-normal text-center p-4">
+    <div className="font-bold font-urbanist flex items-center justify-center">
+      <div className="flex gap-10 text-rishi-black items-center flex-col p-2 justify-center">
+        <div className="w-7/12 font-Urbanist  text-2xl font-normal leading-7 tracking-normal text-center p-4">
           Project RISHI is a nonprofit organization with the mission of
           promoting the sustainable growth and development of the Indian village
           of Ghangar.
         </div>
-        <div className="flex justify-center">
-          <div className=" rounded-full overflow-hidden h-2/3 w-1/2 flex justify-center items-center">
-            <Image src={aboutImage} alt="" className="" />
-          </div>
+        <div className="flex justify-center ">
+          <Image src={aboutImage} width={500} alt="" className="rounded-full" />
         </div>
 
-        <div className="w-1/2 font-Urbanist text-xl font-normal leading-7 tracking-normal text-center p-4">
+        <div className="w-7/12 font-Urbanist text-2xl font-normal leading-7 tracking-normal text-center p-4">
           We partner with local communities and other social enterprises to
           identify issues central to the villages we support and provide them
           resources to implement solutions. In order to do this, we fundraise
