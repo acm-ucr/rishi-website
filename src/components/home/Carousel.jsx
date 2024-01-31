301d0e0a85d3a35f2f65c7721ee03ea7bc18ba93
kevin <kevincao64@gmail.com>
2024-01-30 23:47:55 -0800

    created carousel

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import { carousel } from "@/data/carousel";
+
 const Carousel = () => {
-  return <div>Carousel</div>;
+  return (
+    <div className="relative flex overflow-hidden">
+      <div className="flex animate-marquee object-cover">
+        {carousel.map((image, index) => (
+          <div key={index} className="w-2/4 mx-4">
+            <Image src={image} alt="carousel" className="w-full" />
+          </div>
+        ))}
+      </div>
+      <div className="flex absolute animate-marquee-continuation object-cover">
+        {carousel.map((image, index) => (
+          <div key={index} className="w-2/4 mx-4">
+            <Image src={image} alt="carousel" className="w-full" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,20 @@ module.exports = {
           gray: "#EFEFEF",
         },
       },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "marquee-continuation": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+        "marquee-continuation": "marquee-continuation 30s linear infinite",
+      },
     },
   },
   plugins: [],
