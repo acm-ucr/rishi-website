4d99338195dc79d94aff2a2185c6dc8f01131438
Ricardo Galeano <ricardogaleano@Ricardos-Air-3.home>
2024-02-03 15:50:01 -0800

    fixed Gallery header spacing

--- src/components/gallery/Options.jsx
+++ src/components/gallery/Options.jsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Options = ({ tripClick }) => {
   return (
-    <div className="flex flex-col items-center ">
+    <div className="flex flex-col items-center py-20 gap-y-2">
       <div className="mb-2">
         <button
           onClick={tripClick("2014")}
-          className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline"
+          className="bg-rishi-orange text-rishi-white text-3xl font-medium w-[600px] h-[100px] no-underline"
         >
           INDIA TRIP 2014
         </button>
       </div>
 
       <div className="mb-2">
-        <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
+        <button className="bg-rishi-green text-rishi-white text-3xl font-medium w-[600px] h-[100px] no-underline">
           INDIA TRIP 2016
         </button>
       </div>
 
       <div className="mb-2">
-        <button className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline">
+        <button className="bg-rishi-orange text-rishi-white text-3xl font-medium w-[600px] h-[100px] no-underline">
           INDIA TRIP 2019
         </button>
       </div>
 
       <div className="mb-2">
-        <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
+        <button className="bg-rishi-green text-rishi-white text-3xl font-medium w-[600px] h-[100px] no-underline">
           PAD SHIPMENT 2022
         </button>
       </div>
