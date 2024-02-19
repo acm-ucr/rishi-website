5b205b19674e7a9e51f39d471c7c5eb806ab298f
syedmahdi30 <syedmahdi30@gmail.com>
2024-02-18 18:59:16 -0800

    Fixed sizing issue

--- src/components/board/Member.jsx
+++ src/components/board/Member.jsx
@@ -5,8 +5,8 @@ import { IoMdMail } from "react-icons/io";
 
 const Member = ({ name, role, image, yearMajor, pronouns }) => {
   return (
-    <div className="flex flex-col items-center gap-3 p-5 justify-start bg-rishi-white rounded-xl text-rishi-white shadow-2xl ring-0.5 ring-rishi-black w-48 min-h-full">
-      <div className="relative w-36 h-36 rounded-lg overflow-hidden mt-2">
+    <div className="flex flex-col items-center gap-2 p-5 pb-0 justify-start bg-rishi-white rounded-xl text-rishi-white shadow-2xl ring-0.5 ring-rishi-black min-h-full w-64">
+      <div className="relative w-40 h-40 rounded-lg overflow-hidden mt-2">
         <Image
           src={image || placeholder}
           alt={name}
@@ -14,12 +14,12 @@ const Member = ({ name, role, image, yearMajor, pronouns }) => {
           objectFit="cover"
         />
       </div>
-      <div className="text-left">
-        <p className="text-l font-urbanist font-bold text-rishi-orange tracking-wide">
+      <div className="text-center">
+        <p className="text-2xl font-urbanist font-bold text-rishi-orange tracking-wide">
           {name}
         </p>
-        <p className="text-md font-urbanist text-rishi-black mt-[-5]">{role}</p>
-        <div className="flex gap-2 justify-start">
+        <p className="text-l font-urbanist text-rishi-black mt-[-2]">{role}</p>
+        <div className="flex gap-1 justify-center">
           <FaLinkedinIn className="h-6 w-6 text-rishi-orange" />
           <IoMdMail className="h-6 w-6 text-rishi-orange" />
         </div>
