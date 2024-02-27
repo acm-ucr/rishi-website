2e415eb60ac4eaaf9a63750f14e096ff0effd088
Vidhi Tapde <vidhitapde@Vidhis-MacBook-Pro.local>
2024-02-26 16:52:13 -0800

    responsiveness

--- src/components/gallery/Options.jsx
+++ src/components/gallery/Options.jsx
@@ -2,30 +2,22 @@ import React from "react";
 
 const Options = () => {
   return (
-    <div className="flex flex-col items-center ">
-      <div className="mb-2">
-        <button className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline">
-          INDIA TRIP 2014
-        </button>
-      </div>
+    <div className="flex flex-col items-center gap-2 ">
+      <button className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline">
+        INDIA TRIP 2014
+      </button>
 
-      <div className="mb-2">
-        <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
-          INDIA TRIP 2016
-        </button>
-      </div>
+      <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
+        INDIA TRIP 2016
+      </button>
 
-      <div className="mb-2">
-        <button className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline">
-          INDIA TRIP 2019
-        </button>
-      </div>
+      <button className="bg-rishi-orange text-rishi-white font-semibold py-2 w-60 no-underline">
+        INDIA TRIP 2019
+      </button>
 
-      <div className="mb-2">
-        <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
-          PAD SHIPMENT 2022
-        </button>
-      </div>
+      <button className="bg-rishi-green text-rishi-white font-semibold py-2 w-60 no-underline">
+        PAD SHIPMENT 2022
+      </button>
     </div>
   );
 };
