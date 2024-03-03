622dc4cc7fe5b14fc87a3fc011479c6865bf67d7
syedmahdi30 <syedmahdi30@gmail.com>
2024-03-02 21:38:37 -0800

    Added breakpoints for smaller screens

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -7,17 +7,17 @@ import Link from "next/link";
 const Landing = () => {
   return (
     <div className="flex h-screen ml-6 overflow-x-clip">
-      <div className="flex flex-col w-8/12 justify-center gap-3">
+      <div className="flex flex-col w-8/12 justify-center gap-3 md:gap-0">
         <div className="flex justify-center">
           <Image src={logo} alt="rishi-logo" className="w-24 h-auto" />
         </div>
 
-        <div className="flex justify-center font-urbanist font-extrabold text-rishi-black text-5xl">
+        <div className="flex justify-center font-urbanist font-extrabold text-rishi-black text-4xl md:text-5xl">
           Project RISHI at UCR
         </div>
 
         <div className="flex justify-center">
-          <div className="font-normal text-2xl text-rishi-green text-center">
+          <div className="font-normal text-xl md:text-2xl text-rishi-green text-center">
             Dedicated to promoting sustainable
             <p className="break-before-column">
               growth in the small village Ghangar, India
@@ -35,8 +35,8 @@ const Landing = () => {
         </div>
       </div>
 
-      <div className="flex   items-center relative w-screen lg:-top-36 lg:-right-24 md:-top-52 md:-right-12 sm:-top-80 sm:-right-16 ">
-        <div className="flex   outline outline-rishi-orange outline-3 rounded-full  ">
+      <div className="flex items-center relative w-screen lg:-top-36 lg:-right-24 md:-top-52 md:-right-12 sm:-top-80 sm:-right-16 ">
+        <div className="flex outline outline-rishi-orange outline-3 rounded-full  ">
           <Image
             src={landing}
             alt="landing-page-photo"
