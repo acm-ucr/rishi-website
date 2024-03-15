444bfeac89916f69f4f5e5bea19ad6f992af54c2
syedmahdi30 <syedmahdi30@gmail.com>
2024-03-14 19:07:28 -0700

    Added hover animation

--- src/components/gallery/Options.jsx
+++ src/components/gallery/Options.jsx
@@ -1,34 +1,37 @@
-"use client";
+import { motion } from "framer-motion";
+
 const Options = ({ tripClick }) => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center py-10 gap-y-2 w-1/2">
-        <button
+        <motion.button
+          whileHover={{ scale: 1.1 }}
           onClick={() => tripClick("2014")}
           className="bg-rishi-orange text-rishi-white text-3xl font-medium w-full py-8 no-underline"
         >
           INDIA TRIP 2014
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
           onClick={() => tripClick("2016")}
           className="bg-rishi-green text-rishi-white text-3xl font-medium w-full py-8 no-underline"
         >
           INDIA TRIP 2016
-        </button>
-
-        <button
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
           onClick={() => tripClick("2019")}
           className="bg-rishi-orange text-rishi-white text-3xl font-medium w-full py-8 no-underline"
         >
           INDIA TRIP 2019
-        </button>
-
-        <button
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
           onClick={() => tripClick("2022")}
           className="bg-rishi-green text-rishi-white text-3xl font-medium w-full py-8 no-underline"
         >
           PAD SHIPMENT 2022
-        </button>
+        </motion.button>
       </div>
     </div>
   );
