a4d8000e0165a9c34aeec39c1cd501e8d728836f
theo <theobfernandez@gmail.com>
2024-03-24 10:25:36 -0700

    test

--- src/components/events/CalendarApi.jsx
+++ src/components/events/CalendarApi.jsx
@@ -8,6 +8,7 @@ const CalendarApi = () => {
   const [events, setEvents] = useState();
 
   useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL);
     axios
       .get(
         `https://www.googleapis.com/calendar/v3/calendars/${
